cd5621de7ca32fd21af9e2f2b731780ee34930ca
AK <6294860+ak65234@users.noreply.github.com>
2022-05-17 23:49:22 -0500

    Merge websocket server into one listener

--- config.default.js
+++ config.default.js
@@ -1,4 +1,5 @@
-export const config = {
+module.exports = {
+    port: 3000,
     key: '/path/to/private/key.pem',
     cert: '/path/to/certificate.pem',
     socketPath: '/simmo/socket' // Also update src/client/src/network-controller.js
