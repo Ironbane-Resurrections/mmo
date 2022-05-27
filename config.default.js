fe328edc5130217d6245081ca6b6b13a550db4e2
Some Dev <sumedevz@gmail.com>
2022-05-27 19:24:38 +0100

    OK

--- config.default.js
+++ config.default.js
@@ -1,6 +1,17 @@
-module.exports = {
+var os = require('os')
+if (os.hostname() === 'your_domain_or_ip') {
+  // Server running on server
+  module.exports = {
     port: 3000,
     key: '/path/to/private/key.pem', // Not required for localhost
     cert: '/path/to/certificate.pem', // Not required for localhost
     socketPath: '/simmo/socket' // Also update src/client/src/network-controller.js
+  }
+} else {
+  // Server running on remote Localhost
+  module.exports = {
+    port: 3000,
+    socketPath: '/simmo/socket' // Also update src/client/src/network-controller.js
+  }
 }
+ 
