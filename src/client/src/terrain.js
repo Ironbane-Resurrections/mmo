4905204867b2aa4e535735af9a1624bd6472cc9a
Some Dev <somedev@gmx.com>
2022-10-21 01:23:28 +0100

    Commenting _material.onBeforeCompile fixes terrain

--- src/client/src/terrain.js
+++ src/client/src/terrain.js
@@ -57,7 +57,7 @@ export const terrain = (function () {
         vertexColors: true
       })
 
-      this._material.onBeforeCompile = (s) => {
+ /*      this._material.onBeforeCompile = (s) => {
         //s.isWebGL2 = true
         //s.glslVersion = "300 es"
 
@@ -84,7 +84,7 @@ export const terrain = (function () {
         }
 
         // s.fragmentShader += 'poop';
-      }
+      } */
 
       this._builder = new terrain_builder_threaded.TerrainChunkRebuilder_Threaded()
       // this._builder = new terrain_builder.TerrainChunkRebuilder();
